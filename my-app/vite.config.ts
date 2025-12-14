6a6ac07ff8e6d1a251251a830b40430231b27e1d
nikkokid <buurgaard02@gmail.com>
2025-12-14 16:20:01 +0100

    Initial Commit 2
    Set up ASP .Net 8.0 Core API. Created Dockerfiles for said API, and the React app created in Initial Commit 1. Created docker-compose.yml file which includes the API, React app, and a PostgreSQL DB. Setup environment variable for docker-compose.yml, see .env.example. Created simple init.sql script for DB creation. Created volumes, in docker-compose.yml. and toggled .NET Watcher to enable hot-reload for both React app and ASP .NET Core API.

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 5719,
+  strictPort: true,
+ },
+ server: {
+  port: 5179,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:5179",
+ },
+});
---
+++ postgres/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE grapes (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+INSERT INTO grapes (name) VALUES ('Rondo'), ('Solaris'), ('Cabernet Noir');
